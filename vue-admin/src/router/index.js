0e5724ddee3854e2a6522b631245279e09ecf037
dyj6415 <dyj6415@163.com>
2021-02-03 11:57:01 +0800

    构建项目、路由、elementui

---
+++ vue-admin/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      // redirect: "Login"
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import("../views/Login.vue")
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/About.vue")
+    }
+  ]
+});
